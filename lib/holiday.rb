c6bb7a1a3fee27632ac01208e3364daf7ce167b9
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-27 01:38:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash.each do |attribute, data|
     if attribute == :winter
-      return data
+      return :winter.values
     end
   end
 end
